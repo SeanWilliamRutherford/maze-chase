e402f58e121e28e37aed6de88b7844b7296096ef
SeanWilliamRutherford <swruther@ualberta.ca>
2023-02-02 16:02:34 -0700

    Improved style

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -5,6 +5,8 @@ import {toast} from 'react-toastify'
 import {register, reset} from '../features/auth/authSlice'
 import {FaUser} from 'react-icons/fa'
 
+import styles from '../stylesheets/Login.module.css'
+
 
 
 function Register() {
@@ -62,66 +64,70 @@ function Register() {
 
         return (
             <>
-              <section className='heading'>
-                <h1>
-                  <FaUser /> Register
-                </h1>
-                <p>Please create an account</p>
-              </section>
-        
-              <section className='form'>
-                <form onSubmit={onSubmit}>
-                  <div className='form-group'>
-                    <input
-                      type='text'
-                      className='form-control'
-                      id='name'
-                      name='name'
-                      value={name}
-                      placeholder='Enter your name'
-                      onChange={onChange}
-                    />
-                  </div>
-                  <div className='form-group'>
-                    <input
-                      type='email'
-                      className='form-control'
-                      id='email'
-                      name='email'
-                      value={email}
-                      placeholder='Enter your email'
-                      onChange={onChange}
-                    />
-                  </div>
-                  <div className='form-group'>
-                    <input
-                      type='password'
-                      className='form-control'
-                      id='password'
-                      name='password'
-                      value={password}
-                      placeholder='Enter password'
-                      onChange={onChange}
-                    />
-                  </div>
-                  <div className='form-group'>
-                    <input
-                      type='password'
-                      className='form-control'
-                      id='password2'
-                      name='password2'
-                      value={password2}
-                      placeholder='Confirm password'
-                      onChange={onChange}
-                    />
-                  </div>
-                  <div className='form-group'>
-                    <button type='submit' className='btn btn-block'>
-                      Submit
-                    </button>
-                  </div>
-                </form>
-              </section>
+              <div className={styles.page}>
+
+                <div className={styles.loginform}>
+                  <section className={styles.title}>
+                    <h1>
+                      <FaUser /> Register
+                    </h1>
+                  </section>
+            
+                 
+                    <form onSubmit={onSubmit}>
+                      <div className='form-group'>
+                        <input
+                          type='text'
+                          className={styles.input}
+                          id='name'
+                          name='name'
+                          value={name}
+                          placeholder='Enter your name'
+                          onChange={onChange}
+                        />
+                      </div>
+                      <div className='form-group'>
+                        <input
+                          type='email'
+                          className={styles.input}
+                          id='email'
+                          name='email'
+                          value={email}
+                          placeholder='Enter your email'
+                          onChange={onChange}
+                        />
+                      </div>
+                      <div className='form-group'>
+                        <input
+                          type='password'
+                          className={styles.input}
+                          id='password'
+                          name='password'
+                          value={password}
+                          placeholder='Enter password'
+                          onChange={onChange}
+                        />
+                      </div>
+                      <div className='form-group'>
+                        <input
+                          type='password'
+                          className={styles.input}
+                          id='password2'
+                          name='password2'
+                          value={password2}
+                          placeholder='Confirm password'
+                          onChange={onChange}
+                        />
+                      </div>
+                      <div className={styles.buttonplace}>
+                        <button type='submit' className={styles.button}>
+                          Submit
+                        </button>
+                      </div>
+                    </form>
+                  
+                </div>
+              </div>
             </>
           )
 }
